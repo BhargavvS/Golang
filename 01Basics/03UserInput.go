0dd336c9427031d8eaec44896f8bd873461a239a
Bhargav S <bhargavbharu447@gmail.com>
2026-02-08 12:19:04 +0530

    added commnets to undertand the code in the userinput file

--- 01Basics/03UserInput.go
+++ 01Basics/03UserInput.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
-
 
 func main() {
 	// var myString string;
@@ -15,9 +14,10 @@ func main() {
 	// fmt.Println(myString)
 
 	// var name string ="Bhargav"
-	// var a , _ = fmt.Println(name) // here _ means i know there is some value but doesnot know what it is actually 
+	// var a , _ = fmt.Println(name) // here _ means i know there is some value but doesnot know what it is actually
 	// fmt.Println(a)
 
+	// here we are not able to add +2 to the myRating because => myrating is in string format
 	// reader := bufio.NewReader(os.Stdin)
 	// fmt.Print("Enter your name: ")
 	// mystring, _ := reader.ReadString('\n')
@@ -25,10 +25,10 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter the user rating")
-	myRating , _ := reader.ReadString('\n')
-	mynumRating , _ := strconv.ParseFloat(strings.TrimSpace(myRating), 64) // 64 => the bytes we are considering 
+	myRating, _ := reader.ReadString('\n')
+	mynumRating, _ := strconv.ParseFloat(strings.TrimSpace(myRating), 64) // 64 => the bytes we are considering
 	fmt.Println(mynumRating + 2)
 
-	// here we are using string.trimSpace => parsefloat also consider the \n and the space and 
-	// it cannot be converted to number ,so we have to remove them first and then pass it 
+	// here we are using string.trimSpace => parsefloat also consider the \n and the space and
+	// it cannot be converted to number ,so we have to remove them first and then pass it
 }
